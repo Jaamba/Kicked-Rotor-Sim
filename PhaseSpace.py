bac58b508fd5a9396c7ba5e62312a71335c2b432
Vincenzo Illiano <illianovincenzo05@gmail.com>
2026-03-14 19:15:56 +0100

    Better phase diagram generator

--- PhaseSpace.py
+++ PhaseSpace.py
@@ -5,41 +5,48 @@ import matplotlib.pyplot as plt
 K = 1.2
 
 # numero di iterazioni
-N = 10000
+N = 1000
 
 # numero di tentativi
-M = 30
+M = 20
 
-theta0 = np.linspace(0, np.pi, M)
-p0 = np.random.uniform(-3, 3, M)
+# Crea una griglia di valori iniziali
+theta0 = np.linspace(0, 2*np.pi, M)
+p0 = np.linspace(-np.pi, np.pi, M)
 
+# Colormap per bellezza
 cmap = plt.cm.viridis
 
-for j in range(M):
+# aumenta la risoluzione
+plt.figure(dpi=300)
 
-    theta_list = []
-    p_list = []
+# Cicla sulle condizioni inizali
+for i in range(M):
+    for j in range(M):
 
-    # Sceglie casualmente la condizione iniziale
-    theta = theta0[j]
-    p = p0[j]
+        theta_list = []
+        p_list = []
 
-    # Fa evolvere la j-esima traiettoria
-    for i in range(N):
-    
-        p = p + K*np.sin(theta)
-        theta = theta + p
-    
-        theta = theta % (2*np.pi)
-    
-        theta_list.append(theta)
-        p_list.append(p)
+        # Condizione iniziale
+        theta = theta0[i]
+        p = p0[j]
 
-    # normalizza theta0 in [0,1] per la colormap
-    col = cmap(theta0[j] / (2*np.pi))
+        # Fa evolvere la ij-esima traiettoria
+        for k in range(N):
+        
+            p = p + K*np.sin(theta)
+            theta = theta + p
+        
+            theta = theta % (2*np.pi)
+        
+            theta_list.append(theta)
+            p_list.append(p)
 
-    # Disegna la j-esima traiettoria con un colore casuale
-    plt.scatter(theta_list, p_list, s = 1, color=col)
+        # normalizza theta0 in [0,1] per la colormap
+        col = cmap(theta0[i] / (2*np.pi))
+
+        # Disegna la j-esima traiettoria con un colore casuale
+        plt.scatter(theta_list, p_list, s = 0.1, color=col)
 
 plt.xlabel("theta")
 plt.ylabel("p")
